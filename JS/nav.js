073195259efc064545128eb4f6748fb34cc4abf3
aayushsapkota <96.estd@gmail.com>
2017-05-19 13:27:29 +0930

    changes made in 19/05/2017 1:27pm

--- JS/nav.js
+++ JS/nav.js
@@ -8,3 +8,19 @@ $(".icon").click(function() {
 
 
 });
+
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 130) {
+      $('#myTopnav').addClass('myTopnav_fixed');
+    }
+    if ($(window).scrollTop() < 130) {
+      $('#myTopnav').removeClass('myTopnav_fixed');
+    }
+  });
+});
