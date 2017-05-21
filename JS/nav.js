6b134fa7869061acae183b34d7f1ea01f17900a8
aayushsapkota <96.estd@gmail.com>
2017-05-21 10:44:31 +0930

    final JS and Nav fixes looks perfect
    10 44 21 05 2017

--- JS/nav.js
+++ JS/nav.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-$(".icon").click(function() {
+$(".menubutton").click(function() {
   /* Act on the event */
   $(".topnav").toggleClass('responsive');
 });
@@ -10,17 +10,75 @@ $(".icon").click(function() {
 });
 
 $(document).ready(function() {
-  
+
   $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-      console.log($(window).scrollTop())
+      
+      //trying to know exact nav location when we should switch using console
+     // console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 150) {
+      $('.menubutton').addClass('menubutton_fixed');
+    }
+    if ($(window).scrollTop() < 150) {
+      $('.menubutton').removeClass('menubutton_fixed');
+    }
+      
+      
+   
+});
+});
+$(document).ready(function() {
+
+  $(window).scroll(function () {
+      
+      //trying to know exact nav location when we should switch using console
+     // console.log($(window).scrollTop());
     if ($(window).scrollTop() > 130) {
       $('#myTopnav').addClass('myTopnav_fixed');
     }
     if ($(window).scrollTop() < 130) {
       $('#myTopnav').removeClass('myTopnav_fixed');
     }
-  });
+      
+      
+   
 });
+});
+
+
+$(document).ready(function () {
+		$(document).on("scroll", onScroll);
+ 
+		$('a[href^="#"]').on('click', function (e) {
+			e.preventDefault();
+			$(document).off("scroll");
+ 
+			$('a').each(function () {
+				$(this).removeClass('active');
+			})
+			$(this).addClass('active');
+ 
+			var target = this.hash;
+			$target = $(target);
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top+2
+			}, 500, 'swing', function () {
+				window.location.hash = target;
+				$(document).on("scroll", onScroll);
+			});
+		});
+	});
+ 
+	function onScroll(event){
+		var scrollPosition = $(document).scrollTop();
+		$('nav a').each(function () {
+			var currentLink = $(this);
+			var refElement = $(currentLink.attr("href"));
+			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+				$('nav a').removeClass("active");
+				currentLink.addClass("active");
+			}
+			else{
+				currentLink.removeClass("active");
+			}
+		});
+    };
